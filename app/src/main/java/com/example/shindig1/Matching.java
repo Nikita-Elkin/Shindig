fb4656248f200254f56a646672db1c9212806cc9
Max Knutson <mknutson@imsa.edu>
2018-04-21 16:32:20 -0500

    Login page made responsive

--- app/src/main/java/com/example/shindig1/Matching.java
+++ app/src/main/java/com/example/shindig1/Matching.java
@@ -1,13 +1,18 @@
 package com.example.shindig1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Matching extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_matching_Galaxy5_Pixel1);
+        setContentView(R.layout.activity_matching);
     }
+
 }
