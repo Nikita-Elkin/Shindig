354493559d42ef63dd55b2fe7344bf96bd55e5dd
Nikita-Elkin <38026106+Nikita-Elkin@users.noreply.github.com>
2018-04-21 16:25:13 -0500

    Correct Linking

--- app/src/main/java/com/example/shindig1/Main2Activity.java
+++ app/src/main/java/com/example/shindig1/Main2Activity.java
@@ -12,7 +12,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
