7dce05f8f686df50ca86601f0d38515718246f4c
Max Knutson <mknutson@imsa.edu>
2018-04-27 19:21:19 -0500

    Created the screen upon completing a successful match. The placeholder text that says "Congratulations! You've..." is hardcoded because the actual text is specified in a java file, this just makes it easier to visualize in the layout editor. The java code creates two strings: match_intro and match_name. I seperated these strings through java code to make it easier for Lucien to access and change match_name to the value it needs to be later.

---
+++ app/src/main/java/com/example/shindig1/matched.java
@@ -0,0 +1,25 @@
+package com.example.shindig1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class matched extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matched);
+        String matched_intro = "Congratulations! You\'ve been matched with:";
+        String match_name = "Match Name";
+        String matched_text = matched_intro + " " + match_name;
+
+        TextView matchedTextView = findViewById(R.id.matchTextView);
+        matchedTextView.setText(matched_text);
+    }
+
+    // Lucien needs to make a method to change the match_name string to the actual name of the
+    // match. All I did was separate the constant match introduction and match name to make it
+    // easier for him to do. As of now, match_name will always be the default "Match Name".
+
+}
