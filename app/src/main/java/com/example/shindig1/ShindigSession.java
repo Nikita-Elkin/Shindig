06358744ce066dfb682ac396548e4d8ab892b98f
Nikita-Elkin <38026106+Nikita-Elkin@users.noreply.github.com>
2018-05-16 11:25:42 -0500

    Finished number picker

--- app/src/main/java/com/example/shindig1/ShindigSession.java
+++ app/src/main/java/com/example/shindig1/ShindigSession.java
@@ -3,12 +3,34 @@ package com.example.shindig1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
 
 public class ShindigSession extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.session_questions);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-}}
+
+        NumberPicker np = findViewById(R.id.numberPicker);
+
+        np.setMinValue(0);
+        np.setMaxValue(20);
+
+        np.setOnValueChangedListener(onValueChangeListener);
+
+    }
+
+
+    NumberPicker.OnValueChangeListener onValueChangeListener =
+            new 	NumberPicker.OnValueChangeListener(){
+                @Override
+                public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                    Toast.makeText(ShindigSession.this,
+                            "selected number "+numberPicker.getValue(), Toast.LENGTH_SHORT);
+                }};
+}
