665fb739262eec493cec170ad62e44ba3af0debd
Nikita-Elkin <38026106+Nikita-Elkin@users.noreply.github.com>
2018-05-18 18:41:52 -0500

    Finished Animation for Matching Page

--- app/src/main/java/com/example/shindig1/Matching.java
+++ app/src/main/java/com/example/shindig1/Matching.java
@@ -7,12 +7,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Matching extends AppCompatActivity {
+
+    List<Integer> lstImages= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
+
+        initData();
+
+        HorizontalInfiniteCycleViewPager pager=(HorizontalInfiniteCycleViewPager)findViewById(R.id.horizontal_cycle);
+
+        MyAdapter adapter= new MyAdapter(lstImages,getBaseContext());
+
+        pager.setAdapter(adapter);
+
+
+
+    }
+
+    private void initData(){
+        lstImages.add(R.drawable.card);
+        lstImages.add(R.drawable.card);
+        lstImages.add(R.drawable.card);
     }
 
 }
