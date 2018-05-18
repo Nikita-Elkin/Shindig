d6b949039ac6a3f44d1054022ddf723324b9a0d2
Max Knutson <mknutson@imsa.edu>
2018-05-18 11:18:38 -0500

    Implemented carousel and fixed a few errors

--- app/src/main/java/com/example/shindig1/Matching.java
+++ app/src/main/java/com/example/shindig1/Matching.java
@@ -1,18 +1,66 @@
 package com.example.shindig1;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import anjithsasindran.carouselviewpager.R;
+import anjithsasindran.carouselviewpager.adapter.CarPagerAdapter;
+import anjithsasindran.carouselviewpager.callback.CallbackToActivity;
+import anjithsasindran.carouselviewpager.constant.Constant;
 
-public class Matching extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity implements CallbackToActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_matching);
+        ViewPager mViewPager;
+        CarPagerAdapter carPagerAdapter;
+        ArgbEvaluator argbEvaluator;
+        boolean firstTime = true;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_matching);
+
+            argbEvaluator = new ArgbEvaluator();
+            carPagerAdapter = new CarPagerAdapter(getSupportFragmentManager());
+
+            mViewPager = (ViewPager) findViewById(R.id.pager);
+            mViewPager.setOffscreenPageLimit(2);
+            mViewPager.setAdapter(carPagerAdapter);
+            mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                    // carPagerAdapter.getCount() - 1 because, of using Constant.mutedColor[position] + 1
+                    if (position < carPagerAdapter.getCount() - 1) {
+                        mViewPager.setBackgroundColor((Integer) argbEvaluator.evaluate(positionOffset,
+                                Constant.mutedColors[position], Constant.mutedColors[position + 1]));
+                    }
+                }
+
+                @Override
+                public void onPageSelected(int position) {
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+
+                }
+            });
+        }
+
+        /*
+        Once color is fetched from respective fragments, they are sent back to MainActivity
+        which then sets the colors to adapters.
+         */
+        @Override
+        public void colorFetched(int position, int vibrantColor, int mutedColor) {
+            carPagerAdapter.setPageColor(position, vibrantColor, mutedColor);
+
+            if (firstTime) {
+                mViewPager.setBackgroundColor(mutedColor);
+                firstTime = false;
+            }
+        }
     }
 
 }
